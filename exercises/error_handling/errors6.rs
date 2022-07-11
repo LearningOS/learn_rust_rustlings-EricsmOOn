b207946d9a0f337270399d5c77668d7747c78263
Eric Wong <31581070+EricsmOOn@users.noreply.github.com>
2022-07-11 15:48:36 +0000

    [update] progress 50/84

--- exercises/error_handling/errors6.rs
+++ exercises/error_handling/errors6.rs
@@ -8,8 +8,6 @@
 
 // Make these tests pass! Execute `rustlings hint errors6` for hints :)
 
-// I AM NOT DONE
-
 use std::num::ParseIntError;
 
 // This is a custom error type that we will be using in `parse_pos_nonzero()`.
@@ -21,6 +19,13 @@ enum ParsePosNonzeroError {
 
 impl ParsePosNonzeroError {
     // TODO: add another error conversion function here.
+    fn from_creation(e: CreationError) -> Self {
+        ParsePosNonzeroError::Creation(e)
+    }
+
+    fn parse_int(e: ParseIntError) -> Self {
+        ParsePosNonzeroError::ParseInt(e)
+    }
 }
 
 fn parse_pos_nonzero(s: &str)
@@ -28,9 +33,15 @@ fn parse_pos_nonzero(s: &str)
 {
     // TODO: change this to return an appropriate error instead of panicking
     // when `parse()` returns an error.
-    let x: i64 = s.parse().unwrap();
-    PositiveNonzeroInteger::new(x)
-        .map_err(ParsePosNonzeroError::from_creation)
+    match s.parse() {
+        Ok(v) => {
+            match PositiveNonzeroInteger::new(v) {
+                Ok(v) => Ok(v),
+                Err(e) => Err(ParsePosNonzeroError::from_creation(e)),
+            }
+        }
+        Err(e) => Err(ParsePosNonzeroError::parse_int(e)),
+    }
 }
 
 // Don't change anything below this line.
